820ad142dd5c258c4522c54f09fde0f14fc8842e
Blake <itdallasdbw@gmail.com>
2025-06-20 16:10:44 -0500

    JS 2.4 Loops complete

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-console.log("blake")
+//DRY = Don't Repeat Yourself
+
+// let count = 1;
+// console.log(count);
+// count = count + 1;
+// console.log(count);count = count + 1;
+
+// let count = 1;
+
+// while (count <= 100) {
+//   console.log(count);
+//   count = count + 1;
+// }
+
+// for (let i = 0; i <= 20; i++) {
+//   let FE = i % 3;
+//   let sim = i % 5;
+//   if (FE == 0) {
+//     console.log("Frontend");
+//   }
+//   if (sim == 0) {
+//     console.log("Simplified");
+//   }
+//   if (FE==0 && sim==0) {
+//     console.log("Frontend Simplified")
+//   }
+//   if (FE==0 || sim==0) {
+//     console.log(i)
+//   }
+//   console.log(i);
+// }
+// let f = "Frontend"
+// let s = "Simplified"
+// for (let i = 1; i <= 20; ++i) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log(`${i} -> ${f} ${s}`);
+//   }
+//   else if (i % 3 == 0) {
+//     console.log(`${i} -> ${f}`);
+//   } else if (i % 5 == 0) {
+//     console.log(`${i} -> ${s}`);
+//   } else {
+//     console.log(`${i} -> ${i}`);
+//   }
+// }
+
+let str = "Frontend Simplified"
+// console.log(str[1])
+// console.log(str.length)
+
+for (let i=0; i<str.length; ++i) {
+    console.log(str[i])
+}
